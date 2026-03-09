efab4e080876af853b63fcc73485fd2d3a61dc0a
bcreekman <b@creekman.se>
2026-03-09 03:20:06 +0100

    Finished project

---
+++ asteroid.py
@@ -0,0 +1,33 @@
+import random
+import pygame
+from circleshape import CircleShape
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x,y,radius)
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        vector_one = self.velocity.rotate(angle)
+        vector_two = self.velocity.rotate(-angle)
+        radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_one = Asteroid(self.position.x, self.position.y, radius)
+        asteroid_one.velocity = vector_one
+
+        asteroid_two = Asteroid(self.position.x, self.position.y, radius)
+        asteroid_two.velocity = vector_two
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
---
+++ circleshape.py
@@ -0,0 +1,27 @@
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x,y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def collides_with(self, other):
+        # 1. Calculate the distance between the center of the two circles
+        distance = self.position.distance_to(other.position)
+
+        if distance < self.radius + other.radius:
+            return True
+        else:
+            return False
